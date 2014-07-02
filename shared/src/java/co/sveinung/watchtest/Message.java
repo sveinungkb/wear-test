07234fb32f9ccaff080d35f10663fbe841969f76
Sveinung Kval Bakken <sveinung.bakken@gmail.com>
2014-07-01 21:18:40 -0400

    Added state events

---
+++ shared/src/java/co/sveinung/watchtest/Message.java
@@ -0,0 +1,12 @@
+package co.sveinung.watchtest;
+
+/**
+ * Created by sveinung on 01.07.14.
+ */
+public class Message {
+    public static final String PATH_STATE = "/state";
+    public static final String PATH_CONTROL = "/control";
+    public static final byte CONTROL_STOP = 0;
+    public static final byte CONTROL_PLAY = 1;
+    public static final byte STATE_PLAYING = 1;
+}
--- wear/src/main/java/co/sveinung/watchtest/PlayerActivity.java
+++ wear/src/main/java/co/sveinung/watchtest/PlayerActivity.java
@@ -8,15 +8,23 @@ import android.support.wearable.view.DismissOverlayView;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
 
 /**
  * Created by sveinung on 01.07.14.
@@ -27,6 +35,8 @@ public class PlayerActivity extends InsetActivity {
     private GestureDetector longTouchDetector;
     private MetaView metaView;
     private GoogleApiClient apiClient;
+    private Button actionButton;
+    private boolean nowPlaying;
 
     @Override
     public void onReadyForContent() {
@@ -41,6 +51,9 @@ public class PlayerActivity extends InsetActivity {
             }
         });
         metaView = new MetaView(findViewById(R.id.player_meta));
+        actionButton = (Button) findViewById(R.id.player_action);
+        actionButton.setOnClickListener(onActionClickedListener);
+
         apiClient = new GoogleApiClient.Builder(this, onConnectedListener, onConnectionFailedListener).addApi(Wearable.API).build();
         apiClient.connect();
     }
@@ -51,9 +64,12 @@ public class PlayerActivity extends InsetActivity {
     }
 
     private final GoogleApiClient.ConnectionCallbacks onConnectedListener = new GoogleApiClient.ConnectionCallbacks() {
+
         @Override
         public void onConnected(Bundle bundle) {
             Wearable.DataApi.addListener(apiClient, onDataChangedListener);
+            Wearable.MessageApi.addListener(apiClient, onMessageListener);
+
         }
 
         @Override
@@ -87,4 +103,43 @@ public class PlayerActivity extends InsetActivity {
             }
         }
     };
+
+    public MessageApi.MessageListener onMessageListener = new MessageApi.MessageListener() {
+        @Override
+        public void onMessageReceived(MessageEvent messageEvent) {
+            Log.d(TAG, "onMessageReceived: " + messageEvent);
+            if (Message.PATH_STATE.equals(messageEvent.getPath())) {
+                byte playingState = messageEvent.getData()[0];
+                setNowPlaying(playingState == Message.STATE_PLAYING);
+            }
+        }
+    };
+
+    public void setNowPlaying(boolean nowPlaying) {
+        if (nowPlaying) {
+            actionButton.setText("Stop");
+        }
+        else {
+            actionButton.setText("Play");
+        }
+        this.nowPlaying = nowPlaying;
+    }
+
+    private final View.OnClickListener onActionClickedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            broadcastMessage(Message.PATH_CONTROL, nowPlaying ? Message.CONTROL_STOP : Message.CONTROL_PLAY);
+        }
+    };
+
+    private void broadcastMessage(final String action, final byte payload) {
+        PendingResult<NodeApi.GetConnectedNodesResult> connectedNodes = Wearable.NodeApi.getConnectedNodes(apiClient);
+        List<Node> nodes = connectedNodes.await().getNodes();
+        for (Node node : nodes) {
+            Log.d(TAG, "Sending message to: " + node);
+            Wearable.MessageApi.sendMessage(apiClient, node.getId(), action, new byte[]{payload});
+        }
+    }
+
+
 }
